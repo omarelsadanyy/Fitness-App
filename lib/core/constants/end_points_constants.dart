3633b3fc298df7b762370ec58aabe8a5cd8e82ae
Rana Gebril <ranagebril5@gmail.com>
2025-10-31 13:49:29 +0200

    fix some issues

--- lib/core/constants/end_points_constants.dart
+++ lib/core/constants/end_points_constants.dart
@@ -1,17 +1,20 @@
 abstract class EndPointsConstants {
   static const String baseUrl = "https://fitness.elevateegy.com/api/v1/";
   static const String authEndPoint = "auth/";
-   //signUpEndPoint
-  static const String signUpEndPoint="signup";
+
+  //signUpEndPoint
+  static const String signUpEndPoint = "signup";
   static const String signIn = "signin";
   static const String getLoggedUser = "profile-data";
   static const String forgetPassEndPoint = "forgotPassword";
   static const String verifyResetCode = "verifyResetCode";
   static const String resetPass = "resetPassword";
+
   //exercises
-  static const String difficultyByPrimeMoverMuscle="levels/difficulty-levels/by-prime-mover";
-  static const String exercisesByMuscleAndDifficulty="exercises/by-muscle-difficulty";
-  static const String difficultyLevelId="difficultyLevelId";
-  static const String primeMoverMuscleId="primeMoverMuscleId";
-  static const String page="page";
-  static const String limit="limit";
+  static const String difficultyByPrimeMoverMuscle = "levels/difficulty-levels/by-prime-mover";
+  static const String exercisesByMuscleAndDifficulty = "exercises/by-muscle-difficulty";
+  static const String difficultyLevelId = "difficultyLevelId";
+  static const String primeMoverMuscleId = "primeMoverMuscleId";
+  static const String page = "page";
+  static const String limit = "limit";
+}
